8b2e08422a3b9bafb1ccf783b8767750b33036f4
bigchaoshou <155956986+bigchaoshou@users.noreply.github.com>
2025-04-21 20:11:03 +0300

    Add files via upload

--- Binary_tree2.py
+++ Binary_tree2.py
@@ -1,137 +1,160 @@
-from __future__ import annotations
-from typing import Any, Callable, Iterator, Optional, Tuple, Union
-from typing_extensions import TypedDict
-import itertools
+from typing import Optional, List, Tuple, Callable, Iterator, TypedDict
 
 
 class TreeNodeDict(TypedDict, total=False):
-    key: str
-    value: Any
+    key: int
+    value: str
     left: Optional['TreeNodeDict']
     right: Optional['TreeNodeDict']
 
 
-class BinaryTreeDict:
-    def __init__(self, node: Optional[TreeNodeDict] = None):
+class BSTDictionary:
+    def __init__(self, root: Optional[TreeNodeDict] = None) -> None:
+        self.root = root
+
+    def size(self) -> int:
+        return self._size_recursive(self.root)
+
+    def _size_recursive(self, node: Optional[TreeNodeDict]) -> int:
         if node is None:
-            node = {'key': None, 'value': None, 'left': empty(), 'right': empty()}
-        self.node = node
+            return 0
+        return 1 + self._size_recursive(node['left']) + self._size_recursive(node['right'])
 
-    def is_empty(self) -> bool:
-        return self.node['key'] is None
+    def add(self, key: int, value: str) -> 'BSTDictionary':
+        return BSTDictionary(self._add_recursive(self.root, key, value))
 
-    def add(self, key: str, value: Any) -> BinaryTreeDict:
-        if self.is_empty():
-            return BinaryTreeDict({'key': key, 'value': value, 'left': empty(), 'right': empty()})
-        if key == self.node['key']:
-            return BinaryTreeDict({'key': key, 'value': value, 'left': self.node['left'], 'right': self.node['right']})
-        elif key < self.node['key']:
-            new_left = self.node['left'].add(key, value)
-            return BinaryTreeDict({'key': self.node['key'], 'value': self.node['value'], 'left': new_left, 'right': self.node['right']})
+    def _add_recursive(self, node: Optional[TreeNodeDict], key: int, value: str) -> TreeNodeDict:
+        if node is None:
+            return {'key': key, 'value': value, 'left': None, 'right': None}
+        if key < node['key']:
+            return {
+                'key': node['key'],
+                'value': node['value'],
+                'left': self._add_recursive(node['left'], key, value),
+                'right': node['right']
+            }
+        elif key > node['key']:
+            return {
+                'key': node['key'],
+                'value': node['value'],
+                'left': node['left'],
+                'right': self._add_recursive(node['right'], key, value)
+            }
         else:
-            new_right = self.node['right'].add(key, value)
-            return BinaryTreeDict({'key': self.node['key'], 'value': self.node['value'], 'left': self.node['left'], 'right': new_right})
+            return {
+                'key': key,
+                'value': value,
+                'left': node['left'],
+                'right': node['right']
+            }
 
-    def search(self, key: str) -> Optional[Any]:
-        if self.is_empty():
+    def search(self, key: int) -> Optional[str]:
+        node = self._search_recursive(self.root, key)
+        return node['value'] if node else None
+
+    def _search_recursive(self, node: Optional[TreeNodeDict], key: int) -> Optional[TreeNodeDict]:
+        if node is None or node['key'] == key:
+            return node
+        elif key < node['key']:
+            return self._search_recursive(node['left'], key)
+        else:
+            return self._search_recursive(node['right'], key)
+
+    def set(self, key: int, new_value: str) -> 'BSTDictionary':
+        return self.add(key, new_value)
+
+    def remove(self, key: int) -> 'BSTDictionary':
+        return BSTDictionary(self._delete_recursive(self.root, key))
+
+    def _delete_recursive(self, node: Optional[TreeNodeDict], key: int) -> Optional[TreeNodeDict]:
+        if node is None:
             return None
-        if key == self.node['key']:
-            return self.node['value']
-        elif key < self.node['key']:
-            return self.node['left'].search(key)
+        if key < node['key']:
+            return {
+                'key': node['key'],
+                'value': node['value'],
+                'left': self._delete_recursive(node['left'], key),
+                'right': node['right']
+            }
+        elif key > node['key']:
+            return {
+                'key': node['key'],
+                'value': node['value'],
+                'left': node['left'],
+                'right': self._delete_recursive(node['right'], key)
+            }
         else:
-            return self.node['right'].search(key)
+            if node['left'] is None:
+                return node['right']
+            elif node['right'] is None:
+                return node['left']
+            min_node = self._find_min(node['right'])
+            return {
+                'key': min_node['key'],
+                'value': min_node['value'],
+                'left': node['left'],
+                'right': self._delete_recursive(node['right'], min_node['key'])
+            }
 
-    def member(self, key: str) -> bool:
-        if self.is_empty():
+    def _find_min(self, node: TreeNodeDict) -> TreeNodeDict:
+        while node['left'] is not None:
+            node = node['left']
+        return node
+
+    def member(self, value: str) -> bool:
+        return self._member_recursive(self.root, value)
+
+    def _member_recursive(self, node: Optional[TreeNodeDict], value: str) -> bool:
+        if node is None:
             return False
-        if key == self.node['key']:
+        if node['value'] == value:
             return True
-        elif key < self.node['key']:
-            return self.node['left'].member(key)
-        else:
-            return self.node['right'].member(key)
+        return self._member_recursive(node['left'], value) or self._member_recursive(node['right'], value)
 
-    def remove(self, key: str) -> BinaryTreeDict:
-        if self.is_empty():
-            return self
-        if key < self.node['key']:
-            new_left = self.node['left'].remove(key)
-            return BinaryTreeDict({'key': self.node['key'], 'value': self.node['value'], 'left': new_left, 'right': self.node['right']})
-        elif key > self.node['key']:
-            new_right = self.node['right'].remove(key)
-            return BinaryTreeDict({'key': self.node['key'], 'value': self.node['value'], 'left': self.node['left'], 'right': new_right})
-        else:
-            if self.node['left'].is_empty():
-                return self.node['right']
-            if self.node['right'].is_empty():
-                return self.node['left']
-            min_key, min_value = self.node['right']._find_min()
-            new_right = self.node['right'].remove(min_key)
-            return BinaryTreeDict({'key': min_key, 'value': min_value, 'left': self.node['left'], 'right': new_right})
+    def reverse(self) -> List[Tuple[int, str]]:
+        return list(reversed(self.to_list()))
 
-    def _find_min(self) -> Tuple[str, Any]:
-        if self.node['left'].is_empty():
-            return (self.node['key'], self.node['value'])
-        return self.node['left']._find_min()
+    @classmethod
+    def from_list(cls, lst: List[Tuple[int, str]]) -> 'BSTDictionary':
+        bst = cls.empty()
+        for key, value in lst:
+            bst = bst.add(key, value)
+        return bst
 
-    def to_list(self) -> list[Tuple[str, Any]]:
-        if self.is_empty():
-            return []
-        return self.node['left'].to_list() + [(self.node['key'], self.node['value'])] + self.node['right'].to_list()
+    def to_list(self) -> List[Tuple[int, str]]:
+        result: List[Tuple[int, str]] = []
+        self._inorder_traversal(self.root, result)
+        return result
+
+    def _inorder_traversal(self, node: Optional[TreeNodeDict], result: List[Tuple[int, str]]) -> None:
+        if node is not None:
+            self._inorder_traversal(node['left'], result)
+            result.append((node['key'], node['value']))
+            self._inorder_traversal(node['right'], result)
+
+    def filter(self, predicate: Callable[[int, str], bool]) -> 'BSTDictionary':
+        result = [(k, v) for k, v in self.to_list() if predicate(k, v)]
+        return BSTDictionary.from_list(result)
+
+    def map(self, func: Callable[[int, str], Tuple[int, str]]) -> 'BSTDictionary':
+        mapped = [func(k, v) for k, v in self.to_list()]
+        return BSTDictionary.from_list(mapped)
+
+    def reduce(self, func: Callable[[str, int, str], str], initial_value: str) -> str:
+        result = initial_value
+        for k, v in self.to_list():
+            result = func(result, k, v)
+        return result
+
+    def __iter__(self) -> Iterator[Tuple[int, str]]:
+        return iter(self.to_list())
 
     @staticmethod
-    def from_list(items: list[Tuple[str, Any]]) -> BinaryTreeDict:
-        tree = empty()
-        for k, v in items:
-            tree = tree.add(k, v)
-        return tree
+    def empty() -> 'BSTDictionary':
+        return BSTDictionary()
 
-    def __eq__(self, other: Any) -> bool:
-        return isinstance(other, BinaryTreeDict) and self.to_list() == other.to_list()
-
-    def __str__(self) -> str:
-        return "{" + ", ".join(f"{repr(k)}: {repr(v)}" for k, v in self.to_list()) + "}"
-
-    def __iter__(self) -> Iterator[str]:
-        for k, _ in self.to_list():
-            yield k
-
-    def map(self, f: Callable[[str, Any], Tuple[str, Any]]) -> BinaryTreeDict:
-        return BinaryTreeDict.from_list([f(k, v) for k, v in self.to_list()])
-
-    def filter(self, f: Callable[[str, Any], bool]) -> BinaryTreeDict:
-        return BinaryTreeDict.from_list([(k, v) for k, v in self.to_list() if f(k, v)])
-
-    def reduce(self, f: Callable[[str, str, Any], str], acc: str) -> str:
-        for k, v in self.to_list():
-            acc = f(acc, k, v)
-        return acc
-
-
-def empty() -> BinaryTreeDict:
-    return BinaryTreeDict({'key': None, 'value': None, 'left': None, 'right': None})
-
-
-def intersection(t1: BinaryTreeDict, t2: BinaryTreeDict) -> BinaryTreeDict:
-    result = empty()
-    for k, v in t1.to_list():
-        if t2.member(k):
-            result = result.add(k, v)
-    return result
-
-
-def cons(key: str, value: Any, tree: BinaryTreeDict) -> BinaryTreeDict:
-    return tree.add(key, value)
-
-
-def map_set(tree: BinaryTreeDict, f: Callable[[str], str]) -> BinaryTreeDict:
-    return tree.map(lambda k, v: (f(k), v))
-
-
-def filter_set(tree: BinaryTreeDict, f: Callable[[str], bool]) -> BinaryTreeDict:
-    return tree.filter(lambda k, v: f(k))
-
-
-def reduce_set(tree: BinaryTreeDict, f: Callable[[Any, str, Any], Any], acc: Any) -> Any:
-    return tree.reduce(f, acc)
+    def concat(self, other: 'BSTDictionary') -> 'BSTDictionary':
+        result = self
+        for key, value in other:
+            result = result.add(key, value)
+        return result
