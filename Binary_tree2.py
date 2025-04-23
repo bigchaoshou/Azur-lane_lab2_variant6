560bbc516f5bfeacc06b16710b68bd68ab0d5b72
bigchaoshou <155956986+bigchaoshou@users.noreply.github.com>
2025-04-23 20:53:20 +0300

    Add files via upload

--- Binary_tree2.py
+++ Binary_tree2.py
@@ -43,13 +43,17 @@ class BinaryTreeDict(Generic[KT, VT]):
     def _size_recursive(self, node: Optional[TreeNodeDict]) -> int:
         if node is None:
             return 0
-        return 1 + self._size_recursive(node['left']) + self._size_recursive(node['right'])
+        return (1
+                + self._size_recursive(node['left'])
+                + self._size_recursive(node['right']))
 
     def add(self, key: KT, value: VT) -> 'BinaryTreeDict[KT, VT]':
         """添加/更新键值对，返回新字典"""
         return BinaryTreeDict(self._add_recursive(self.root, key, value))
 
-    def _add_recursive(self, node: Optional[TreeNodeDict], key: KT, value: VT) -> TreeNodeDict:
+    def _add_recursive(
+            self, node: Optional[TreeNodeDict],
+            key: KT, value: VT) -> TreeNodeDict:
         if node is None:
             return {'key': key, 'value': value, 'left': None, 'right': None}
 
@@ -81,7 +85,9 @@ class BinaryTreeDict(Generic[KT, VT]):
         node = self._search_recursive(self.root, key)
         return node['value'] if node else None
 
-    def _search_recursive(self, node: Optional[TreeNodeDict], key: KT) -> Optional[TreeNodeDict]:
+    def _search_recursive(self,
+                          node: Optional[TreeNodeDict],
+                          key: KT) -> Optional[TreeNodeDict]:
         if node is None:
             return None
         cmp = compare_keys(key, node['key'])
@@ -96,15 +102,19 @@ class BinaryTreeDict(Generic[KT, VT]):
         """检查键是否存在"""
         return self._search_recursive(self.root, key) is not None
 
-    def set_value(self, key: KT, new_value: VT) -> 'BinaryTreeDict[KT, VT]':
+    def set_value(self, key: KT,
+                  new_value: VT) -> 'BinaryTreeDict[KT, VT]':
         """更新键对应的值，返回新字典"""
         return self.add(key, new_value)
 
-    def remove(self, key: KT) -> 'BinaryTreeDict[KT, VT]':
+    def remove(self,
+               key: KT) -> 'BinaryTreeDict[KT, VT]':
         """删除指定键的条目，返回新字典"""
         return BinaryTreeDict(self._delete_recursive(self.root, key))
 
-    def _delete_recursive(self, node: Optional[TreeNodeDict], key: KT) -> Optional[TreeNodeDict]:
+    def _delete_recursive(self,
+                          node: Optional[TreeNodeDict],
+                          key: KT) -> Optional[TreeNodeDict]:
         if node is None:
             return None
 
@@ -177,7 +187,8 @@ class BinaryTreeDict(Generic[KT, VT]):
             result.append((node['key'], node['value']))
             self._inorder_traversal(node['right'], result)
 
-    def filter(self, predicate: Callable[[KT], bool]
+    def filter(self,
+               predicate: Callable[[KT], bool]
                ) -> 'BinaryTreeDict[KT, VT]':
         """过滤键值对生成新字典"""
         result = [(k, v) for k, v in self.to_list() if predicate(k)]
@@ -214,7 +225,9 @@ class BinaryTreeDict(Generic[KT, VT]):
         items = self.to_list()
         if not items:
             return "{}"
-        return "{" + ", ".join(f"{repr(k)}: {repr(v)}" for k, v in items) + "}"
+        return "{" + ", ".join(
+            f"{repr(k)}: {repr(v)}" for k, v in items
+        ) + "}"
 
     def __eq__(self, other: object) -> bool:
         """判断两个字典是否相等"""
@@ -228,19 +241,18 @@ class BinaryTreeDict(Generic[KT, VT]):
         return BinaryTreeDict()
 
 
-# ------------------------------
-# 全局辅助函数（适配测试用例）
-# ------------------------------
-
 def empty() -> BinaryTreeDict[None, None]:
     return BinaryTreeDict.empty()
 
 
-def cons(key: Any, value: Any, tree: BinaryTreeDict[Any, Any]) -> BinaryTreeDict[Any, Any]:
+def cons(key: Any,
+         value: Any,
+         tree: BinaryTreeDict[Any, Any]) -> BinaryTreeDict[Any, Any]:
     return tree.add(key, value)
 
 
-def concat(t1: BinaryTreeDict[Any, Any], t2: BinaryTreeDict[Any, Any]) -> BinaryTreeDict[Any, Any]:
+def concat(t1: BinaryTreeDict[Any, Any],
+           t2: BinaryTreeDict[Any, Any]) -> BinaryTreeDict[Any, Any]:
     return t1.concat(t2)
 
 
@@ -248,15 +260,18 @@ def member(key: Any, tree: BinaryTreeDict[Any, Any]) -> bool:
     return tree.contains_key(key)
 
 
-def remove(tree: BinaryTreeDict[Any, Any], key: Any) -> BinaryTreeDict[Any, Any]:
+def remove(tree: BinaryTreeDict[Any, Any],
+           key: Any) -> BinaryTreeDict[Any, Any]:
     return tree.remove(key)
 
 
-def from_list(items: Iterable[Tuple[Any, Any]]) -> BinaryTreeDict[Any, Any]:
+def from_list(items: Iterable[Tuple[Any, Any]]
+              ) -> BinaryTreeDict[Any, Any]:
     return BinaryTreeDict.from_list(items)
 
 
-def to_list(tree: BinaryTreeDict[Any, Any]) -> List[Tuple[Any, Any]]:
+def to_list(tree: BinaryTreeDict[Any, Any]
+            ) -> List[Tuple[Any, Any]]:
     return tree.to_list()
 
 
@@ -264,7 +279,9 @@ def length(tree: BinaryTreeDict[Any, Any]) -> int:
     return tree.size()
 
 
-def intersection(t1: BinaryTreeDict[Any, Any], t2: BinaryTreeDict[Any, Any]) -> BinaryTreeDict[Any, Any]:
+def intersection(t1: BinaryTreeDict[Any, Any],
+                 t2: BinaryTreeDict[Any, Any]
+                 ) -> BinaryTreeDict[Any, Any]:
     result = empty()
     for k, v in t1.to_list():
         if t2.contains_key(k):
@@ -272,13 +289,19 @@ def intersection(t1: BinaryTreeDict[Any, Any], t2: BinaryTreeDict[Any, Any]) -> 
     return result
 
 
-def filter_set(tree: BinaryTreeDict[Any, Any], predicate: Callable[[Any, Any], bool]) -> BinaryTreeDict[Any, Any]:
+def filter_set(tree: BinaryTreeDict[Any, Any],
+               predicate: Callable[[Any, Any], bool]
+               ) -> BinaryTreeDict[Any, Any]:
     return tree.filter(predicate)
 
 
-def map_set(tree: BinaryTreeDict[Any, Any], func: Callable[[Any, Any], Tuple[Any, Any]]) -> BinaryTreeDict[Any, Any]:
+def map_set(tree: BinaryTreeDict[Any, Any],
+            func: Callable[[Any, Any],
+            Tuple[Any, Any]]) -> BinaryTreeDict[Any, Any]:
     return tree.map(func)
 
 
-def reduce_set(tree: BinaryTreeDict[Any, Any], func: Callable[[Any, Any, Any], Any], initial: Any) -> Any:
+def reduce_set(tree: BinaryTreeDict[Any, Any],
+               func: Callable[[Any, Any, Any], Any],
+               initial: Any) -> Any:
     return tree.reduce(func, initial)
