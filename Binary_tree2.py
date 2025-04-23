b9600890ce41da61f8a85575342fb14a87061c16
Futano <2395604522@qq.com>
2025-04-23 04:37:50 +0300

    加入类型

--- Binary_tree2.py
+++ Binary_tree2.py
@@ -1,251 +1,220 @@
-from typing import (
-    Optional, List, Tuple, Callable,
-    Iterator, TypedDict, Generic, TypeVar, Iterable
-)
-
-# 定义类型变量
-KT = TypeVar('KT', int, None)  # 键类型仅允许int
-VT = TypeVar('VT', str, None)  # 值类型完全泛型
-AccT = TypeVar('AccT', int, None)  # 累加器类型泛型
+from __future__ import annotations
+from typing import Any, Callable, Generic, Iterator, Optional, Tuple, TypeVar
+from typing_extensions import Protocol
 
 
-class TreeNodeDict(TypedDict, total=False):
-    """二叉搜索树节点字典结构"""
-    key: KT
-    value: VT
-    left: Optional['TreeNodeDict']
-    right: Optional['TreeNodeDict']
+class SupportsRichComparison(Protocol):
+    def __lt__(self, other: Any) -> bool: ...
+    def __le__(self, other: Any) -> bool: ...
+    def __gt__(self, other: Any) -> bool: ...
+    def __ge__(self, other: Any) -> bool: ...
 
 
-class BSTDictionary(Generic[KT, VT]):
-    """不可变二叉搜索树字典"""
 
-    def __init__(self, root: Optional[TreeNodeDict] = None) -> None:
-        self.root = root
+KT = TypeVar("KT", int, str)
+VT = TypeVar("VT", int, str)
+AccT = TypeVar("AccT")
 
-    def size(self) -> int:
-        """获取字典条目数"""
-        return self._size_recursive(self.root)
 
-    def _size_recursive(self, node: Optional[TreeNodeDict]) -> int:
+class BinaryTreeDict(Generic[KT, VT]):
+    def __init__(self, node: Optional[dict] = None):
         if node is None:
-            return 0
-        return 1 + self._size_recursive(node['left']) + self._size_recursive(node['right'])
-
-    def add(self, key: KT, value: VT) -> 'BSTDictionary[KT, VT]':
-        """添加/更新键值对，返回新字典"""
-        return BSTDictionary(self._add_recursive(self.root, key, value))
-
-    def _add_recursive(self, node: Optional[TreeNodeDict], key: KT, value: VT) -> TreeNodeDict:
-        if node is None:
-            return {'key': key, 'value': value, 'left': None, 'right': None}
-
-        if key < node['key']:
-            return {
-                'key': node['key'],
-                'value': node['value'],
-                'left': self._add_recursive(node['left'], key, value),
-                'right': node['right']
-            }
-        elif key > node['key']:
-            return {
-                'key': node['key'],
-                'value': node['value'],
-                'left': node['left'],
-                'right': self._add_recursive(node['right'], key, value)
-            }
+            self.node = _EMPTY_NODE
         else:
-            return {
+            self.node = node
+
+    def is_empty(self) -> bool:
+        return self.node['key'] is None
+
+    def add(self, key: KT, value: Optional[VT] = None) -> (
+            BinaryTreeDict)[KT, VT]:
+        if self.is_empty():
+            return BinaryTreeDict({'key': key, 'value': value,
+                                   'left': empty(), 'right': empty()})
+
+        if key == self.node['key']:
+            return BinaryTreeDict({
                 'key': key,
                 'value': value,
-                'left': node['left'],
-                'right': node['right']
-            }
+                'left': self.node['left'],
+                'right': self.node['right']
+            })
+        elif key < self.node['key']:
+            return BinaryTreeDict({
+                'key': self.node['key'],
+                'value': self.node['value'],
+                'left': self.node['left'].add(key, value),
+                'right': self.node['right']
+            })
+        else:
+            return BinaryTreeDict({
+                'key': self.node['key'],
+                'value': self.node['value'],
+                'left': self.node['left'],
+                'right': self.node['right'].add(key, value)
+            })
 
     def search(self, key: KT) -> Optional[VT]:
-        """搜索指定键的值"""
-        node = self._search_recursive(self.root, key)
-        return node['value'] if node else None
-
-    def _search_recursive(self, node: Optional[TreeNodeDict], key: KT) -> Optional[TreeNodeDict]:
-        if node is None or node['key'] == key:
-            return node
-        return self._search_recursive(node['left'], key) if key < node['key'] else self._search_recursive(node['right'],
-                                                                                                          key)
-
-    def set_value(self, key: KT, new_value: VT) -> 'BSTDictionary[KT, VT]':
-        """更新键对应的值，返回新字典"""
-        return self.add(key, new_value)
-
-    def remove(self, key: KT) -> 'BSTDictionary[KT, VT]':
-        """删除指定键的条目，返回新字典"""
-        return BSTDictionary(self._delete_recursive(self.root, key))
-
-    def _delete_recursive(self, node: Optional[TreeNodeDict], key: KT) -> Optional[TreeNodeDict]:
-        if node is None:
+        if self.is_empty():
             return None
-
-        if key < node['key']:
-            return {
-                'key': node['key'],
-                'value': node['value'],
-                'left': self._delete_recursive(node['left'], key),
-                'right': node['right']
-            }
-        elif key > node['key']:
-            return {
-                'key': node['key'],
-                'value': node['value'],
-                'left': node['left'],
-                'right': self._delete_recursive(node['right'], key)
-            }
+        if key == self.node['key']:
+            return self.node['value']
+        elif key < self.node['key']:
+            return self.node['left'].search(key)
         else:
-            if node['left'] is None:
-                return node['right']
-            if node['right'] is None:
-                return node['left']
+            return self.node['right'].search(key)
 
-            # 找到右子树最小节点
-            min_node = self._find_min(node['right'])
-            return {
-                'key': min_node['key'],
-                'value': min_node['value'],
-                'left': node['left'],
-                'right': self._delete_recursive(node['right'], min_node['key'])
-            }
-
-    def _find_min(self, node: TreeNodeDict) -> TreeNodeDict:
-        """查找子树最小节点"""
-        while node['left'] is not None:
-            node = node['left']
-        return node
-
-    def member(self, value: VT) -> bool:
-        """检查值是否存在"""
-        return self._member_recursive(self.root, value)
-
-    def _member_recursive(self, node: Optional[TreeNodeDict], value: VT) -> bool:
-        if node is None:
+    def member(self, key: KT) -> bool:
+        if self.is_empty():
             return False
-        return (
-                node['value'] == value or
-                self._member_recursive(node['left'], value) or
-                self._member_recursive(node['right'], value)
-        )
+        if key == self.node['key']:
+            return True
+        elif key < self.node['key']:
+            return self.node['left'].member(key)
+        else:
+            return self.node['right'].member(key)
 
-    @classmethod
-    def from_list(cls, lst: Iterable[Tuple[KT, VT]]) -> 'BSTDictionary[KT, VT]':
-        """从可迭代对象创建字典"""
-        bst = cls.empty()
-        for key, value in lst:
-            bst = bst.add(key, value)
-        return bst
+    def remove(self, key: KT) -> BinaryTreeDict[KT, VT]:
+        if self.is_empty():
+            return self
+        if key < self.node['key']:
+            new_left = self.node['left'].remove(key)
+            return BinaryTreeDict({'key': self.node['key'],
+                                   'value': self.node['value'],
+                                   'left': new_left,
+                                   'right': self.node['right']})
+        elif key > self.node['key']:
+            new_right = self.node['right'].remove(key)
+            return BinaryTreeDict({'key': self.node['key'],
+                                   'value': self.node['value'],
+                                   'left': self.node['left'],
+                                   'right': new_right})
+        else:
+            if self.node['left'].is_empty():
+                return self.node['right']
+            if self.node['right'].is_empty():
+                return self.node['left']
+            min_key, min_value = self.node['right']._find_min()
+            new_right = self.node['right'].remove(min_key)
+            return BinaryTreeDict({'key': min_key, 'value': min_value,
+                                   'left': self.node['left'],
+                                   'right': new_right})
 
-    def to_list(self) -> List[Tuple[KT, VT]]:
-        """转换为有序键值对列表"""
-        result: List[Tuple[KT, VT]] = []
-        self._inorder_traversal(self.root, result)
-        return result
+    def _find_min(self) -> Tuple[KT, VT]:
+        if self.node['left'].is_empty():
+            return (self.node['key'], self.node['value'])
+        return self.node['left']._find_min()
 
-    def _inorder_traversal(self, node: Optional[TreeNodeDict], result: List[Tuple[KT, VT]]) -> None:
-        if node is not None:
-            self._inorder_traversal(node['left'], result)
-            result.append((node['key'], node['value']))
-            self._inorder_traversal(node['right'], result)
-
-    def filter(self, predicate: Callable[[KT, VT], bool]) -> 'BSTDictionary[KT, VT]':
-        result = [(k, v) for k, v in self.to_list() if predicate(k, v)]
-        return BSTDictionary.from_list(result)
-
-    def map(self, func: Callable[[KT, VT], Tuple[KT, VT]]) -> 'BSTDictionary[KT, VT]':
-        """映射键值对生成新字典"""
-        mapped = [func(k, v) for k, v in self.to_list()]
-        return BSTDictionary.from_list(mapped)
-
-    def reduce(self, func: Callable[[AccT, KT, VT], AccT], initial: AccT) -> AccT:
-        """归约操作"""
-        result = initial
-        for k, v in self.to_list():
-            result = func(result, k, v)
-        return result
-
-    def concat(self, other: 'BSTDictionary[KT, VT]') -> 'BSTDictionary[KT, VT]':
-        """合并两个字典"""
-        new_tree = self
-        for key, value in other:
-            new_tree = new_tree.add(key, value)
-        return new_tree
-
-    def __iter__(self) -> Iterator[Tuple[KT, VT]]:
-        """迭代器支持"""
-        return iter(self.to_list())
+    def to_list(self) -> list[Tuple[KT, VT]]:
+        if self.is_empty():
+            return []
+        return (self.node['left'].to_list() +
+                [(self.node['key'],
+                  self.node['value'])]
+                + self.node['right'].to_list())
 
     @staticmethod
-    def empty() -> 'BSTDictionary[KT, VT]':
-        """创建空字典"""
-        return BSTDictionary()
+    def from_list(items: list[Tuple[KT, VT]]) \
+            -> BinaryTreeDict[KT, VT]:
+        tree = empty()
+        for k, v in items:
+            tree = tree.add(k, v)
+        return tree
+
+    def __eq__(self, other: Any) -> bool:
+        return (isinstance(other, BinaryTreeDict) and
+                sorted(self.to_list()) == sorted(other.to_list()))
+
+    def __str__(self) -> str:
+        return "{" + ", ".join(f"{repr(k)}: {repr(v)}"
+                               for k, v in sorted(self.to_list(),
+                                                  key=lambda x:
+                                                  (x[0] is not None,
+                                                   x[0]))) + "}"
+
+    def __iter__(self) -> Iterator[KT]:
+        for k, _ in self.to_list():
+            yield k
+
+    def map(self, f: Callable[[KT], KT]) \
+            -> BinaryTreeDict[KT, VT]:
+        return BinaryTreeDict.from_list([(f(k), v)
+                                         for k, v in self.to_list()])
+
+    def filter(self, f: Callable[[KT], bool]) \
+            -> BinaryTreeDict[KT, VT]:
+        return BinaryTreeDict.from_list([(k, v) for
+                                         k, v in self.to_list() if f(k)])
+
+    def reduce(self, f: Callable[[AccT, KT], AccT],
+               acc: AccT) -> AccT:
+        for k, _ in self.to_list():
+            acc = f(acc, k)
+        return acc
 
 
-# 全局辅助函数
-def empty() -> BSTDictionary[int, str]:
-    """创建空字典（键类型为int，值类型为str）"""
-    return BSTDictionary.empty()
+_EMPTY_NODE: dict = {'key': None, 'value': None, 'left': None, 'right': None}
+_EMPTY_TREE: BinaryTreeDict = BinaryTreeDict(_EMPTY_NODE)
+_EMPTY_NODE['left'] = _EMPTY_TREE
+_EMPTY_NODE['right'] = _EMPTY_TREE
 
 
-def cons(key: int, value: str, tree: BSTDictionary[int, str]) -> BSTDictionary[int, str]:
-    """添加键值对"""
+def empty() -> BinaryTreeDict[KT, VT]:
+    return _EMPTY_TREE
+
+
+def cons(key: KT, value: VT, tree: BinaryTreeDict[KT, VT]) \
+        -> BinaryTreeDict[KT, VT]:
     return tree.add(key, value)
 
 
-def concat(t1: BSTDictionary[int, str], t2: BSTDictionary[int, str]) -> BSTDictionary[int, str]:
-    """合并两个字典"""
-    return t1.concat(t2)
+def concat(t1: BinaryTreeDict[KT, VT], t2: BinaryTreeDict[KT, VT]) \
+        -> BinaryTreeDict[KT, VT]:
+    return BinaryTreeDict.from_list(t1.to_list() + t2.to_list())
 
 
-def member(value: str, tree: BSTDictionary[int, str]) -> bool:
-    """检查值是否存在"""
-    return tree.member(value)
+def length(tree: BinaryTreeDict[KT, VT]) -> int:
+    return len(tree.to_list())
 
 
-def remove(tree: BSTDictionary[int, str], key: int) -> BSTDictionary[int, str]:
-    """删除指定键"""
+def member(key: KT, tree: BinaryTreeDict[KT, VT]) \
+        -> bool:
+    return tree.member(key)
+
+
+def remove(tree: BinaryTreeDict[KT, VT], key: KT) \
+        -> BinaryTreeDict[KT, VT]:
     return tree.remove(key)
 
 
-def intersection(t1: BSTDictionary[int, str], t2: BSTDictionary[int, str]) -> BSTDictionary[int, str]:
-    """获取键交集字典"""
+def from_list(items: list[Tuple[KT, VT]]) \
+        -> BinaryTreeDict[KT, VT]:
+    return BinaryTreeDict.from_list(items)
+
+
+def to_list(tree: BinaryTreeDict[KT, VT]) -> list[Tuple[KT, VT]]:
+    return tree.to_list()
+
+
+def intersection(t1: BinaryTreeDict[KT, VT], t2: BinaryTreeDict[KT, VT])\
+        -> BinaryTreeDict[KT, VT]:
     result = empty()
-    for k, v in t1.to_list():
-        if t2.search(k) is not None:
+    for k, v in to_list(t1):
+        if t2.member(k):
             result = result.add(k, v)
     return result
 
 
-def from_list(items: Iterable[Tuple[int, str]]) -> BSTDictionary[int, str]:
-    """从列表创建字典"""
-    return BSTDictionary.from_list(items)
+def map_set(tree: BinaryTreeDict[KT, None], f: Callable[[KT], KT]) \
+        -> BinaryTreeDict[KT, None]:
+    return tree.map(f)
 
 
-def to_list(tree: BSTDictionary[int, str]) -> List[Tuple[int, str]]:
-    """转换为列表"""
-    return tree.to_list()
+def filter_set(tree: BinaryTreeDict[KT, None], f: Callable[[KT], bool]) \
+        -> BinaryTreeDict[KT, None]:
+    return tree.filter(f)
 
 
-def map_func(tree: BSTDictionary[int, str], func: Callable[[int, str], Tuple[int, str]]) -> BSTDictionary[int, str]:
-    """映射操作"""
-    return tree.map(func)
-
-
-def filter_func(tree: BSTDictionary[int, str], predicate: Callable[[int, str], bool]) -> BSTDictionary[int, str]:
-    """过滤操作"""
-    return tree.filter(predicate)
-
-
-def reduce_func(tree: BSTDictionary[int, str], func: Callable[[AccT, int, str], AccT], initial: AccT) -> AccT:
-    """归约操作"""
-    return tree.reduce(func, initial)
-
-
-def size(tree: BSTDictionary[int, str]) -> int:
-    """获取字典大小"""
-    return tree.size()
+def reduce_set(tree: BinaryTreeDict[KT, None], f: Callable[[AccT, KT], AccT],
+               acc: AccT) -> AccT:
+    return tree.reduce(f, acc)
--- test_api.py
+++ test_api.py
@@ -1,7 +1,9 @@
 import itertools
+from hypothesis import given, strategies as st
 from Binary_tree2 import (
     BinaryTreeDict,
     cons,
+    empty,
     length,
     remove,
     member,
@@ -14,7 +16,7 @@ from Binary_tree2 import (
     reduce_set
 )
 
-
+    
 def test_api():
     empty_tree = BinaryTreeDict()
 
@@ -94,3 +96,131 @@ def test_api():
 
     total_len = reduce_set(set1, lambda acc, k: acc + len(k), 0)
     assert total_len == len("a") + len("b") + len("c")
+
+
+
+keys = st.integers()
+values = st.text()
+key_value_pairs = st.tuples(keys, values)
+
+# ------------------------- Basic Functionality Tests -------------------------
+@given(pairs=st.lists(key_value_pairs))
+def test_add_and_length(pairs):
+    tree = empty()
+    unique_keys = set()
+    expected_length = 0
+    for k, v in pairs:
+        if k not in unique_keys:
+            unique_keys.add(k)
+            expected_length += 1
+        tree = tree.add(k, v)
+        assert len(tree.to_list()) == expected_length  # 使用 to_list() 长度替代 length()
+
+@given(pairs=st.lists(key_value_pairs), key=st.integers())
+def test_search(pairs, key):
+    tree = from_list(pairs)
+    # 预期值：最后一个出现的 key 对应的 value
+    expected_value = next((v for k, v in reversed(pairs) if k == key), None)
+    assert tree.search(key) == expected_value
+
+@given(pairs=st.lists(key_value_pairs), key=st.integers(), new_value=st.text())
+def test_add_overwrite(pairs, key, new_value):
+    tree = from_list(pairs)
+    updated_tree = tree.add(key, new_value)
+    assert updated_tree.search(key) == new_value
+
+# ------------------------- Remove & Member Tests -------------------------
+@given(pairs=st.lists(key_value_pairs), key=st.integers())
+def test_remove(pairs, key):
+    tree = from_list(pairs)
+    unique_keys = {k for k, _ in pairs}
+    removed_tree = remove(tree, key)
+    if key in unique_keys:
+        assert not member(key, removed_tree)
+        assert len(removed_tree.to_list()) == len(unique_keys) - 1
+    else:
+        assert removed_tree == tree  # 未修改原树
+
+@given(pairs=st.lists(key_value_pairs), key=st.integers())
+def test_member(pairs, key):
+    tree = from_list(pairs)
+    unique_keys = {k for k, _ in pairs}
+    assert member(key, tree) == (key in unique_keys)
+
+# ------------------------- List Conversion Tests -------------------------
+@given(pairs=st.lists(key_value_pairs))
+def test_from_list_to_list(pairs):
+    tree = from_list(pairs)
+    # 去重并保留最后一次出现的键
+    expected_dict = {}
+    for k, v in pairs:
+        expected_dict[k] = v
+    expected = sorted(expected_dict.items(), key=lambda x: x[0])
+    assert tree.to_list() == expected
+
+# ------------------------- Higher-Order Functions Tests -------------------------
+@given(pairs=st.lists(key_value_pairs))
+def test_filter(pairs):
+    tree = from_list(pairs)
+    filtered = tree.filter(lambda k: k % 2 == 0)
+    expected = [(k, v) for k, v in tree.to_list() if k % 2 == 0]
+    assert filtered.to_list() == expected
+
+@given(pairs=st.lists(key_value_pairs))
+def test_map(pairs):
+    tree = from_list(pairs)
+    mapped = tree.map(lambda k: k + 1)
+    expected = [(k + 1, v) for k, v in tree.to_list()]
+    assert mapped.to_list() == sorted(expected, key=lambda x: x[0])
+
+@given(pairs=st.lists(key_value_pairs))
+def test_reduce(pairs):
+    tree = from_list(pairs)
+    sum_keys = tree.reduce(lambda acc, k: acc + k, 0)
+    expected = sum(k for k, _ in tree.to_list())
+    assert sum_keys == expected
+
+# ------------------------- Iterator Test -------------------------
+@given(pairs=st.lists(key_value_pairs))
+def test_iterator(pairs):
+    tree = from_list(pairs)
+    via_iterator = list(iter(tree))
+    assert sorted(via_iterator) == sorted(k for k, _ in tree.to_list())
+
+# ------------------------- Empty Tree Test -------------------------
+def test_empty():
+    tree = empty()
+    assert tree.is_empty()
+    assert tree.to_list() == []
+
+# ------------------------- Concatenation Tests -------------------------
+@given(pairs1=st.lists(key_value_pairs), pairs2=st.lists(key_value_pairs))
+def test_concat(pairs1, pairs2):
+    t1 = from_list(pairs1)
+    t2 = from_list(pairs2)
+    combined = concat(t1, t2)
+    # 预期结果：t2 的键覆盖 t1 的同名键
+    expected_dict = dict(pairs1 + pairs2)
+    expected = sorted(expected_dict.items(), key=lambda x: x[0])
+    assert combined.to_list() == expected
+
+@given(pairs1=st.lists(key_value_pairs), pairs2=st.lists(key_value_pairs), pairs3=st.lists(key_value_pairs))
+def test_concat_associativity(pairs1, pairs2, pairs3):
+    t1 = from_list(pairs1)
+    t2 = from_list(pairs2)
+    t3 = from_list(pairs3)
+    # (t1 + t2) + t3
+    left_assoc = concat(concat(t1, t2), t3)
+    # t1 + (t2 + t3)
+    right_assoc = concat(t1, concat(t2, t3))
+    assert left_assoc == right_assoc
+
+# ------------------------- Monoid Laws Test -------------------------
+@given(pairs=st.lists(key_value_pairs))
+def test_monoid_laws(pairs):
+    t = from_list(pairs)
+    empty_tree = empty()
+    # 左单位元
+    assert concat(empty_tree, t) == t
+    # 右单位元
+    assert concat(t, empty_tree) == t
